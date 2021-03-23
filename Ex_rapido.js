40fa0960aab792e6f97b9256860227a0b5f470f7
xandenunes <xandenunes302@outlook.com>
2021-03-22 22:40:50 -0300

    feat: adicionado ex_rapido

---
+++ Ex_rapido.js
@@ -0,0 +1,44 @@
+// 1
+let weight=120;
+// 2
+console.log(typeof weight);
+// 3 
+    let  name="xande";
+    let  age=19;
+    let  stars= 4.5;
+    let  isSubscribed= true;
+// 4.0
+    // let student={}
+    // console.log(typeof student);
+//4.1
+let student={
+    weight:120,
+    name:"xande",
+    age:19,
+    stars: 4.5,
+    isSubscribed: true
+}
+//4.2
+console.log(`${student.name} de idade ${student.age} pesa ${student.weight} kg`);
+//5
+    let students = [];
+//6
+students = [
+    student
+];
+console.log(students)
+//7
+console.log(students[0])
+//8
+let jhon={
+    weight:80,
+    name:"jucas",
+    age:20,
+    stars: 5.5,
+    isSubscribed: true
+}
+students[1]=jhon;
+console.log(students[1]);
+//9
+console.log(a);
+var a=1;
